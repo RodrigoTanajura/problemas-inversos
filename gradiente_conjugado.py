3d6a06d7a80d3694ad783d7d1fc3911c280ea3ce
Rodrigo Tanajura <rod.tanajura@gmail.com>
2025-12-07 14:52:36 -0300

    Refactor: atualizando inputs no GC

--- gradiente_conjugado.py
+++ gradiente_conjugado.py
@@ -1,21 +1,25 @@
 import numpy as np
-from problema_direto import problema_direto, pseudo_dados, problema_adjunto
+from problema_direto import problema_direto, pseudo_dados, problema_adjunto, problema_sensibilidade
+from problema_direto import N
 
+L1_0 = 0
+L2_0 = 0
 
 def objective_functional(T1, T2, Y1, Y2):
     result = np.sum((T1 - Y1)**2) + np.sum((T2 - Y2)**2)
 
     return result
 
-def gradiente_conjudado(eps, P0=[1.0, 0.2, 10.0, 0.2], version='fs'):
+def gradiente_conjugado(eps, u_0=np.ones(N), version='fs'):
     i = 0
+    u = u_0
     P0 = np.array([P0])
     i_max = 100
     grad_old = 1
     dk_old = 0
     while (i < i_max):
         # Step 1
-        t, T1, T2 = problema_direto(T1=T1, T2=T2, *P0)
+        t, T1, T2 = problema_direto(u, T1=1.0, T2=1.0)
         t, Y1, Y2 = pseudo_dados()
 
         # Step 2
@@ -24,8 +28,8 @@ def gradiente_conjudado(eps, P0=[1.0, 0.2, 10.0, 0.2], version='fs'):
         if S_g < eps:
             break
 
-        # Step 3: solve adjoint problem
-        t, L1, L2 = problema_adjunto(T1, T2, Y1, Y2)
+        # Step 3: solve adjoint problem. T e Y serão invertidos dentro da função.
+        t, L1, L2 = problema_adjunto(L1_0, L2_0, T1, T2, Y1, Y2)
 
         # Step 4: compute the gradient of S
         grad = -L1
@@ -35,6 +39,7 @@ def gradiente_conjudado(eps, P0=[1.0, 0.2, 10.0, 0.2], version='fs'):
             y_k  = (np.sum((grad)**2))/ np.sum(((grad_old)**2)) if i !=0 else 0
             psi_k = 0
             dk_q = 0
+
         elif version == 'pr':
             psi_k = 0
             dk_q = 0
@@ -49,6 +54,7 @@ def gradiente_conjudado(eps, P0=[1.0, 0.2, 10.0, 0.2], version='fs'):
         d_k = -grad + (y_k * dk_old) + psi_k * dk_q
         grad_old = grad
         # Step 6: set Deltag equal to dk and solve the sensitivity problem
-        delta_g = d_k
+        delta_u = d_k
 
+        t, delta_T1, delta_T2 = problema_sensibilidade()
     return
